54a6015b7adc54f275f6a68eae746aa96c061d87
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-02-09 11:38:49 -0500

    send response: Anki Deck

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('*', (re,res)=>{
+    res.send('Anki Deck')
+})
+
 const PORT = process.env.PORT || 5999
 
 app.listen(PORT, ()=> console.log(`Server Listening on Port: ${PORT}`))
